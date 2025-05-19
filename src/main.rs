8dec6b18733c435940c24cbb1017d5bed43297f6
gv <afairless@gmail.com>
2025-05-18 23:19:48 -0400

    duplicated plot horizontally side-by-side

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,51 @@ mod plot;
 
 #[function_component(App)]
 pub fn plot_component() -> Html {
-    let p = use_async::<_, _, ()>({
-        let id = "plot-div";
+
+    let p1 = use_async::<_, _, ()>({
+        let id = "plot1";
+        let (plot, line_traces) = plot::create_plot();
+        // Wrap plot in Rc<RefCell<>> for shared ownership
+        let plot = Rc::new(RefCell::new(plot)); 
+
+        async move {
+            // Borrow the inner Plot
+            plotly::bindings::new_plot(id, &*plot.borrow()).await; 
+
+            // Animate the lines
+            for (i, trace) in line_traces.into_iter().enumerate() {
+                let delay = i as u32 * 1000; // 1 second per animation step
+                let window = window().unwrap();
+                let id = id.to_string();
+                let plot = Rc::clone(&plot); // Clone Rc for the closure
+                let closure = Closure::wrap(Box::new(move || {
+                    let id = id.clone();
+                    let trace = trace.clone();
+                    let plot = Rc::clone(&plot); // Clone Rc for plot
+                    spawn_local(async move {
+                        // Borrow mutably for add_trace
+                        let mut plot = plot.borrow_mut(); 
+                        plot.add_trace(Box::new(*trace));
+                        plotly::bindings::react(&id, &plot).await;
+                    });
+                }) as Box<dyn Fn()>);
+
+                window
+                    .set_timeout_with_callback_and_timeout_and_arguments_0(
+                        closure.as_ref().unchecked_ref(),
+                        delay as i32,
+                    )
+                    .unwrap();
+
+                closure.forget(); // Prevent memory leaks
+            }
+
+            Ok(())
+        }
+    });
+
+    let p2 = use_async::<_, _, ()>({
+        let id = "plot2";
         let (plot, line_traces) = plot::create_plot();
         // Wrap plot in Rc<RefCell<>> for shared ownership
         let plot = Rc::new(RefCell::new(plot)); 
@@ -53,11 +96,17 @@ pub fn plot_component() -> Html {
     });
 
     use_effect_with((), move |_| {
-        p.run();
+        p1.run();
+        p2.run();
     });
 
+    // html! {
+    //  <div id="plot-div"></div>
     html! {
-        <div id="plot-div"></div>
+        <div style="display: flex; justify-content: space-around;">
+            <div id="plot1" style="width: 50%;"></div>
+            <div id="plot2" style="width: 50%;"></div>
+        </div>
     }
 }
 
